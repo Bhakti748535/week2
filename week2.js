49e59515630cc00f3fe74ca3780b3e58c6c57ea4
Bhakti Bhakti <748535@student.tor.loc>
2020-01-15 16:16:11 -0500

    1st

---
+++ week2.js
@@ -0,0 +1,23 @@
+/*count=1083;
+sum=0;
+countdown =88;
+for (;count>22;)
+{
+    sum=sum+count;
+    count=count-countdown;
+   
+    console.log(sum);
+}*/
+
+
+
+count=1083;
+sum=0;
+countdown =88;
+while (count>22)
+{
+    sum=sum+count;
+    count=count-countdown;
+   
+    console.log(sum);
+}
